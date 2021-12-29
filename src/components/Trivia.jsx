38ffe01a9cb8dbe95334a791f99d886a34143281
Luis Chilo <gfxargentina@gmail.com>
2021-12-29 19:54:34 -0300

    trivia styles

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Trivia = () => {
+  return (
+    <div className="trivia">
+      <div className="question">Pregunta va aqui</div>
+      <div className="answers">
+        <div className="answer">Respuesta va aqui</div>
+        <div className="answer correct">Respuesta va aqui</div>
+        <div className="answer">Respuesta va aqui</div>
+        <div className="answer wrong">Respuesta va aqui</div>
+      </div>
+    </div>
+  );
+};
+
+export default Trivia;
